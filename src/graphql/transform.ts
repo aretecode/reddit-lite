00fdc00bf62827293636041ca4b708bffc05df0d
aretecode <aretecode@gmail.com>
2019-05-14 20:09:43 -0700

    use date-fns in transform

--- src/graphql/transform.ts
+++ src/graphql/transform.ts
@@ -1,3 +1,4 @@
+import { distanceInWords, format } from 'date-fns'
 import { RedditBasicResponseType, RedditLitePostItemType, RedditLitePostKindType } from '../typings'
 
 export function fromResponseToPostList(response: RedditBasicResponseType) {
@@ -5,6 +6,9 @@ export function fromResponseToPostList(response: RedditBasicResponseType) {
     (child): RedditLitePostItemType => {
       const {data} = child
 
+      /**
+       * could separate these to their own lines
+       */
       return {
         id: data.id,
         postKind: (data.post_hint || 'text') as RedditLitePostKindType,
@@ -20,6 +24,15 @@ export function fromResponseToPostList(response: RedditBasicResponseType) {
         authorFullName: data.author_fullname || '',
         authorFlairText: data.author_flair_text || '',
         commentCount: data.num_comments || 0,
+
+        /**
+         * we could also add another for hover
+         */
+        createdAtUtc: data.created_utc,
+        createdAtIso: format(data.created_utc * 1000, 'YYYY-MM-DD[T]HH:mm:ssZZ'),
+        createdAtPretty: distanceInWords(data.created_utc * 1000, Date.now()),
+
+        imageUrl: data.thumbnail === 'self' ? '' : data.thumbnail,
       }
     }
   )
