fc9cddfcdff4947db18daefaf820c7335a68d394
aretecode <aretecode@gmail.com>
2019-05-14 17:07:44 -0700

    project basics (lint, configs)

---
+++ globals.d.ts
@@ -0,0 +1,31 @@
+// if we import this, it fails
+// import { NormalizedCacheObject } from 'apollo-boost'
+interface NormalizedCacheObject {
+  [key: string]: unknown
+}
+
+declare global {
+  interface Window {
+    readonly __APOLLO_STATE__: NormalizedCacheObject
+    readonly __APP_STATE__: {[key: string]: unknown}
+  }
+}
+
+interface Window {
+  readonly __APOLLO_STATE__: {[key: string]: unknown}
+  readonly __APP_STATE__: {[key: string]: unknown}
+}
+
+declare namespace NodeJS {
+  interface Global {
+    process: Process
+  }
+  interface ProcessEnv {
+    NODE_ENV: 'test' | 'development' | 'production'
+    IS_NOW: 'true' | 'false' | undefined
+  }
+  interface Process {
+    browser: boolean
+    env: ProcessEnv
+  }
+}
