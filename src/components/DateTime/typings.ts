c694504556c8a912c9bf1e46e150cd6e7a28e382
aretecode <aretecode@gmail.com>
2019-05-15 20:18:38 -0700

    add className to DateTime typings

--- src/components/DateTime/typings.ts
+++ src/components/DateTime/typings.ts
@@ -3,6 +3,7 @@ import * as React from 'react'
 export interface DateTimeBaseProps {
   children: string | React.ReactChildren
   dateTime: string
+  className?: string
 }
 export type DateTimeProps = DateTimeBaseProps &
   React.TimeHTMLAttributes<any> & { className: string }
