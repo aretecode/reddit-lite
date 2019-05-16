d38ddb221073d2fdd45be066b84fee5fb5c41b55
aretecode <aretecode@gmail.com>
2019-05-15 21:32:07 -0700

    ! deploy storybook with now

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -6,35 +6,40 @@ module.exports = baseConfig => {
   /**
    * these rules make storybook slow, so they are removed
    */
-  config.module.rules = config.module.rules.filter(loader => {
-    if (
-      Array.isArray(loader.include) &&
-      loader.include.some(
-        x => typeof x === 'string' && x.includes('.storybook')
-      )
-    ) {
-      return false
-    } else if (Array.isArray(loader.use) === false || loader.use.length === 0) {
-      return true
-    } else if (
-      (Array.isArray(loader.use[0].plugins) &&
-        (loader.use[0].plugins.some(x => Array.isArray(x)) === true ||
-          // remove dupe babel
-          loader.use[0].plugins.includes('@babel/plugin-syntax-jsx'))) ||
-      JSON.stringify(loader).includes('DOC_GEN_COLLECTION_NAME')
-    ) {
-      console.warn('removed storybook babel')
-      return false
-    } else if (
-      loader.use.some(x => x.options && !!x.options.indent) ||
-      JSON.stringify(loader).includes('postcss')
-    ) {
-      // remove postcss
-      return false
-    } else {
-      return true
-    }
-  })
+  if (process.env.NODE_ENV === 'development') {
+    config.module.rules = config.module.rules.filter(loader => {
+      if (
+        Array.isArray(loader.include) &&
+        loader.include.some(
+          x => typeof x === 'string' && x.includes('.storybook')
+        )
+      ) {
+        return false
+      } else if (
+        Array.isArray(loader.use) === false ||
+        loader.use.length === 0
+      ) {
+        return true
+      } else if (
+        (Array.isArray(loader.use[0].plugins) &&
+          (loader.use[0].plugins.some(x => Array.isArray(x)) === true ||
+            // remove dupe babel
+            loader.use[0].plugins.includes('@babel/plugin-syntax-jsx'))) ||
+        JSON.stringify(loader).includes('DOC_GEN_COLLECTION_NAME')
+      ) {
+        console.warn('removed storybook babel')
+        return false
+      } else if (
+        loader.use.some(x => x.options && !!x.options.indent) ||
+        JSON.stringify(loader).includes('postcss')
+      ) {
+        // remove postcss
+        return false
+      } else {
+        return true
+      }
+    })
+  }
 
   /**
    * @see https://www.styled-components.com/docs/tooling#babel-plugin
@@ -73,16 +78,23 @@ module.exports = baseConfig => {
     ],
   })
 
-  const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
-  const plugin = new ForkTsCheckerWebpackPlugin({
-    // async: false,
-    tsconfig: require.resolve('../tsconfig.json'),
-    tslint: require.resolve('../tslint.json'),
-    measureCompilationTime: true,
-    useTypescriptIncrementalApi: true,
-    checkSyntacticErrors: true,
-  })
-  config.plugins.push(plugin)
+  if (process.env.NODE_ENV === 'development') {
+    const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+    const plugin = new ForkTsCheckerWebpackPlugin({
+      // async: false,
+      tsconfig: require.resolve('../tsconfig.json'),
+      tslint: require.resolve('../tslint.json'),
+      measureCompilationTime: true,
+      useTypescriptIncrementalApi: true,
+      checkSyntacticErrors: true,
+    })
+    config.plugins.push(plugin)
+  } else {
+    config.output = {
+      ...config.output,
+      publicPath: '/ui',
+    }
+  }
   config.node = {
     dgram: 'empty',
     fs: 'empty',
