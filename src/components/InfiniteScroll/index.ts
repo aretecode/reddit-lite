5534eff640f402ced7a8fee04d60a65de15bfa82
aretecode <aretecode@gmail.com>
2019-05-15 19:39:41 -0700

    delete InfiniteScroll for simplicity

--- src/components/InfiniteScroll/index.ts
+++
@@ -1,4 +0,0 @@
-export { InfiniteScroll as default } from './InfiniteScroll'
-export { InfiniteScroll } from './InfiniteScroll'
-export { StyledInfiniteScrollWrap } from './styled'
-export * from './typings'
